9540ddcb6cfb393231ddd8712ef85cd2912f9e5d
Sergey Obidin <player181285@gmail.com>
2022-01-24 15:22:20 +0400

    added new tests

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -10,7 +10,10 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.DataGenerator.Registration.generateUser;
 import static ru.netology.DataGenerator.Registration.registeredUser;
+import static ru.netology.DataGenerator.getLoginFaker;
+import static ru.netology.DataGenerator.getPasswordFaker;
 
 
 public class AuthTest {
@@ -28,8 +31,45 @@ public class AuthTest {
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $(byText("Личный кабинет")).shouldBe(visible);
+        $(byText("личный кабинет")).shouldBe(visible);
     }
 
+    @Test
+    public void shouldSendFullFormWithBlockedUser() {
+        UserInfo blockedUser = registeredUser ("blocked"); // передача статуса пользователя
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+//        $(byText("заблокирован")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldSendFullFormWithUnregisteredUser() {
+        UserInfo unregisteredUser = generateUser ("active"); // передача статуса пользователя
+        $("[data-test-id='login'] input").setValue(unregisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(unregisteredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+//        $(byText("не зарегестрирован")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldSendFullFormWithRegisteredUserLoginError() {
+        UserInfo registeredUser =  registeredUser ("active"); // передача статуса пользователя
+        String loginError = getLoginFaker();
+        $("[data-test-id='login'] input").setValue(loginError);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+//        $(byText("неверный логин")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldSendFullFormWithRegisteredUserPasswordError() {
+        UserInfo registeredUser =  registeredUser ("active"); // передача статуса пользователя
+        String passwordFaker = getPasswordFaker();
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(passwordFaker);
+        $("[data-test-id='action-login']").click();
+//        $(byText("неверный пароль")).shouldBe(visible);
+    }
 
 }
